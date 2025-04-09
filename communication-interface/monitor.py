cd2232ffaf25cadf9dbcc31297e582c3f3131636
conisch <cabarriga@uc.cl>
2025-04-09 15:30:54 +0000

    folder path fic

--- communication-interface/monitor.py
+++ communication-interface/monitor.py
@@ -13,7 +13,7 @@ MEMORY_SIZE = 0x20000        # Tamaño de memoria
 STREAM_REGISTER_SIZE = 4     # Tamaño del registro (en bytes)
 
 # Leer el archivo JSON de configuracion
-with open('/root/communication-interface/config.json', 'r') as file:
+with open('/root/droplet-sorting-FPGA-controller/communication-interface/config.json', 'r') as file:
     vars_data = json.load(file)
 vars_to_send = vars_data["variables_to_send"]
 
@@ -163,22 +163,26 @@ def save_logs_periodically():
     while True:
         time.sleep(flush_interval)  # Esperar antes de intentar guardar
         with lock:
-            if log_buffer:
-                with open('/root/communication-interface/registers_data.txt', 'a') as log_file:
-                    for entry in log_buffer:
+            log_buffer_copy = log_buffer
+            if log_buffer_copy:
+                with open('/root/droplet-sorting-FPGA-controller/communication-interface/registers_data.txt', 'a') as log_file:
+                    for entry in log_buffer_copy:
                         log_file.write(json.dumps(entry) + '\n')
                 log_buffer.clear()
+                log_buffer_copy.clear()
 
 def save_voltage_periodically():
     """Thread worker para guardar logs de voltaje en un archivo."""
     while True:
         time.sleep(flush_interval)  # Esperar antes de intentar guardar
-        temp_path = "/root/communication-interface/voltage_data.json.tmp"  # Archivo temporal
-        if log_voltage:
+        temp_path = "/root/droplet-sorting-FPGA-controller/communication-interface/voltage_data.json.tmp"  # Archivo temporal
+        log_voltage_copy = log_voltage
+        if log_voltage_copy:
             with open(temp_path, "w", encoding="utf-8") as file:
-                json.dump(log_voltage, file) 
+                json.dump(log_voltage_copy, file) 
             log_voltage.clear()
-            os.replace(temp_path, "/root/communication-interface/voltage_data.json")
+            log_voltage_copy.clear()
+            os.replace(temp_path, "/root/droplet-sorting-FPGA-controller/communication-interface/voltage_data.json")
 
 
 # Inicializar memoria mapeada fuera del bucle
@@ -186,7 +190,7 @@ try:
     with open("/dev/mem", "r+b") as f:
         mem = mmap.mmap(f.fileno(), MEMORY_SIZE, offset=MEMORY_ADDRESS)
         # Vaciar el archivo (sobrescribe el contenido)
-        with open('/root/communication-interface/registers_data.txt', 'w') as log_file:
+        with open('/root/droplet-sorting-FPGA-controller/communication-interface/registers_data.txt', 'w') as log_file:
             log_file.truncate(0) 
 
         # Inicia el thread para guardar datos
